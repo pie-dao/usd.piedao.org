7fbc6fb3cb3ef4e7f52c94b75cf51f3b14bc9104
alexintosh <alessio.d@gmail.com>
2020-07-06 16:21:03 +0200

    Remove whitepaper link

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -33,14 +33,6 @@ const Header = ({ images, links }) => {
         >
           docs
         </a>
-        <a
-          className="link"
-          href={whitepaper}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          whitepaper
-        </a>
         <ConnectButton />
         <div className="mobile-placeholder" />
 
@@ -71,17 +63,6 @@ const Header = ({ images, links }) => {
                     rel="noopener noreferrer"
                   >
                     docs
-                  </a>
-                </li>
-                <li>
-                  <a
-                    onClick={toggle}
-                    className="navbar-item"
-                    href={whitepaper}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    whitepaper
                   </a>
                 </li>
                 <li>
